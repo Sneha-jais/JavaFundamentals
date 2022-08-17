be9f6ed5fb1a71856cb174ce707c3d53f70705aa
Sneha Jaiswal <78521605+Sneha-jais@users.noreply.github.com>
2022-08-17 19:31:58 +0530

    Create Fahrenheit_to_celsius_table.java

---
+++ ConditionalsAndLoops/Fahrenheit_to_celsius_table.java
@@ -0,0 +1,28 @@
+
+import java.util.Scanner;
+
+public class Solution {
+
+
+	public static void main(String[] args) {
+		
+		/* Your class should be named Solution.
+	 	* Read input as specified in the question.
+	 	* Print output as specified in the question.
+		*/
+    Scanner s=new Scanner(System.in);
+    int S=s.nextInt();
+    int E=s.nextInt();
+    int W=s.nextInt();
+	int F;
+        
+        while(S<=E)
+        {
+         
+            F=((5*(S-32))/9);
+             System.out.println(S+" "+F);
+            S+=W;
+        }
+	}
+
+}
