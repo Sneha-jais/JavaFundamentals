6c94e9be10447def0e9d906a4bb6190d9caf2f93
Sneha Jaiswal <78521605+Sneha-jais@users.noreply.github.com>
2022-08-19 21:33:29 +0530

    Update Average_marks.java

--- GettingStartedWithJava/Average_marks.java
+++ GettingStartedWithJava/Average_marks.java
@@ -1,3 +1,11 @@
+/*
+Write a program to input a name(as a single character) and marks of three tests as m1, m2, and m3 of a student considering all the three marks have been given in integer format.
+Now, you need to calculate the average of the given marks and print it along with the name as mentioned in the output format section. :)
+for input : A
+	    3 4 6
+output will be : A
+		 4
+*/
 
 import java.util.Scanner;
 public class Solution{
