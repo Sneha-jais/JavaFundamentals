1c221c77becff4680168fbfb4d5c2520e0b422b5
Sneha Jaiswal <78521605+Sneha-jais@users.noreply.github.com>
2022-09-01 00:26:37 +0530

    Create count_words.java

---
+++ Strings/count_words.java
@@ -0,0 +1,27 @@
+/* :)
+For a given input string(str), find and return the total number of words present in it.
+It is assumed that two words will have only a single space in between. Also, there wouldn't be any leading and trailing spaces in the given input string.
+Sample Input:
+this is a sample string
+Sample Output:
+5
+*/
+
+
+public class Solution {
+
+	public static int countWords(String str) {	
+		//Your code goes here
+        if(str.length()<1){
+            return 0;
+        }
+        int count=1;
+        for(int i=0;i<str.length();i++){
+            if(str.charAt(i)==' '){
+                count++;
+            }
+        }
+        return count;
+	}
+
+}
