8f51bfac77f3cf166720ba1c8e94487f70319c7d
Sneha Jaiswal <78521605+Sneha-jais@users.noreply.github.com>
2022-12-07 01:10:19 +0530

    Update Multiplication_table.java

--- ConditionalsAndLoops/Multiplication_table.java
+++ ConditionalsAndLoops/Multiplication_table.java
@@ -1,8 +1,11 @@
+
 import java.util.Scanner;
+
 public class Main {
 	
 	public static void main(String[] args) {
 		// Write your code here
+		
         Scanner s= new Scanner(System.in);
         int n=s.nextInt();
         
