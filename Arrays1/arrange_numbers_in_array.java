3cac33d5344b25d95704e27b2e6b38cdd4c35579
Sneha Jaiswal <78521605+Sneha-jais@users.noreply.github.com>
2023-04-29 23:47:21 +0530

    Update arrange_numbers_in_array.java

--- Arrays1/arrange_numbers_in_array.java
+++ Arrays1/arrange_numbers_in_array.java
@@ -1,4 +1,5 @@
-/* You have been given an empty array(ARR) and its size N. :) 
+/*
+You have been given an empty array(ARR) and its size N. :) 
 The only input taken from the user will be N and you need not worry about the array.
 Your task is to populate the array using the integer values in the range 1 to N(both inclusive) in the order - 1,3,5,.......,6,4,2.
 input: 
@@ -8,6 +9,7 @@ input:
 output:
 1 3 5 7 9 8 6 4 2
 1 3 2
+
 */
 
 
