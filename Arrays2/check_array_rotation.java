57a6628963dd7d4ddef65dd4f28283d11a399eb2
Sneha Jaiswal <78521605+Sneha-jais@users.noreply.github.com>
2023-05-18 23:50:35 +0530

    Update check_array_rotation.java

--- Arrays2/check_array_rotation.java
+++ Arrays2/check_array_rotation.java
@@ -2,12 +2,14 @@
 You have been given an integer array/list(ARR) of size N. It has been sorted(in increasing order) and then rotated by some number 'K' in the right hand direction.
 Your task is to write a function that returns the value of 'K', that means, the index from which the array/list has been rotated.
 Sample Input:
+
 1
 6
 5 6 1 2 3 4
 Sample Output:
 2
 */
+
 
 public class Solution {
 
@@ -19,6 +21,7 @@ public class Solution {
         if(n<1){
             return 0;
         }
+        
         for(int i=0;i<n-1;i++){
             if(arr[i]>arr[i+1]){
                 x=i+1;
